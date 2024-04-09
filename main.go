23b9908536632515710889d0a5030b89b08422c7
Mikhail Oleynik <mishutka@mito-team.com>
2024-04-09 15:26:40 +0300

    index web router handler

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 	"github.com/mitoteam/goappbase"
 	"github.com/mitoteam/mt-checklist/app"
 )
@@ -13,6 +16,10 @@ func main() {
 	application.LongDescription = `Checklists management system`
 
 	application.AppSettings = &app.AppSettingsType{}
+
+	application.BuildWebRouterF = func(r *gin.Engine) {
+		r.GET("/", func(c *gin.Context) { c.String(http.StatusOK, "index.html") })
+	}
 
 	application.Run()
 }
