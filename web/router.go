13cd9576c0dd615b8544dce2b80f03efc127a074
Mikhail Oleynik <mishutka@mito-team.com>
2024-04-15 18:44:20 +0300

    header added to html templates, auth drafting

--- web/router.go
+++ web/router.go
@@ -3,8 +3,10 @@ package web
 import (
 	"html/template"
 	"net/http"
+	"slices"
 
 	"github.com/gin-contrib/multitemplate"
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"github.com/mitoteam/mt-checklist/app"
 )
@@ -16,7 +18,7 @@ func BuildWebRouter(r *gin.Engine) {
 	r.StaticFS("/assets", webAssetsHttpFS)
 
 	//serve HTML from templates
-	inc_templates := []string{"inc/base.html", "inc/footer.html"}
+	inc_templates := []string{"inc/base.html", "inc/header.html", "inc/footer.html"}
 
 	render := multitemplate.NewRenderer()
 	render.Add("index", template.Must(template.ParseFS(templatesFS, append(inc_templates, "index.html")...)))
@@ -25,8 +27,30 @@ func BuildWebRouter(r *gin.Engine) {
 
 	r.HTMLRender = render
 
+	r.Use(authMiddleware([]string{"/logout"}))
 	r.GET("/", webIndex)
-	r.GET("/login", webLogin)
+	r.GET("/logout", webLogout)
+}
+
+// checks if user authenticated, redirects to /login if not (except for excludedPaths).
+func authMiddleware(excludedPaths []string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if slices.Contains(excludedPaths, c.FullPath()) {
+			//no auth required for route
+			c.Next()
+		}
+
+		session := sessions.Default(c)
+
+		if session.Get("userID") == nil {
+			webLogin(c)
+			c.Abort() //stop other handlers
+			return
+		}
+
+		// Call the next handler
+		c.Next()
+	}
 }
 
 func webIndex(c *gin.Context) {
@@ -40,5 +64,14 @@ func webIndex(c *gin.Context) {
 }
 
 func webLogin(c *gin.Context) {
-	c.HTML(http.StatusOK, "login", gin.H{})
+	c.HTML(http.StatusOK, "login", gin.H{"App": app.App})
+}
+
+func webLogout(c *gin.Context) {
+	session := sessions.Default(c)
+
+	session.Delete("userID")
+	session.Save()
+
+	c.Redirect(http.StatusFound, "/")
 }
