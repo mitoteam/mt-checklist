a127c5a9a597203509d07156ec8f6f09d4b456be
Mikhail Oleynik <mishutka@mito-team.com>
2024-12-13 14:37:13 +0300

    render stats draft

--- web/pages.go
+++ web/pages.go
@@ -2,6 +2,7 @@ package web
 
 import (
 	"github.com/mitoteam/dhtml"
+	"github.com/mitoteam/mt-checklist/model"
 	"github.com/mitoteam/mtweb"
 )
 
@@ -17,41 +18,42 @@ func PageFormExperiment(p *PageBuilder) bool {
 }
 
 func PageDashboard(p *PageBuilder) bool {
-	cards_list := mtweb.NewCardList().
-		Add(
-			mtweb.NewCard().Header(mtweb.Icon("vial").Label("Experiment")).
-				Body(dhtml.Div().Text("Html renderer ").Append(dhtml.NewLink("/experiment").Label("experiment")).Text(" link.")).
-				Body(
-					dhtml.Div().Text("Confirm link ").Append(dhtml.NewConfirmLink("/experiment", "Are you sure?").Label("experiment")),
-				).
-				Body(dhtml.Div().Text("Forms ").Append(dhtml.NewLink("/form").Label("experiment")).Text(" link.")),
-		).
-		Add(
-			mtweb.NewCard().Header(mtweb.Icon("list-check").Label("Active checklists")).
-				Body("Some content"),
-		).
-		Add(
-			mtweb.NewCard().Header(mtweb.Icon("user-check").Label("My issues")).
-				Body("Some content"),
-		).
-		Add(
-			mtweb.NewCard().Header(mtweb.Icon("chart-simple").Label("Statistics")).
-				Body("Some content"),
-		).
-		Add(
-			mtweb.NewCard().Header(mtweb.Icon("cog").Label("System management")).
-				Body(dhtml.Div().Append(
-					dhtml.NewLink("/admin/users").Label(mtweb.Icon("user").Label("Users")),
-				)).
-				Body(dhtml.Div().Append(
-					dhtml.Div().Append(
-						dhtml.NewLink("/admin/checklists").Label(mtweb.Icon("list-check").Label("Checklists")),
-					).Append(" (administration)"),
-				)),
-		)
+	cards_list := mtweb.NewCardList().Add(
+		mtweb.NewCard().Header(mtweb.Icon("vial").Label("Experiment")).
+			Body(dhtml.Div().Text("Html renderer ").Append(dhtml.NewLink("/experiment").Label("experiment")).Text(" link.")).
+			Body(
+				dhtml.Div().Text("Confirm link ").Append(dhtml.NewConfirmLink("/experiment", "Are you sure?").Label("experiment")),
+			).
+			Body(dhtml.Div().Text("Forms ").Append(dhtml.NewLink("/form").Label("experiment")).Text(" link.")),
+	).Add(
+		mtweb.NewCard().Header(mtweb.Icon("list-check").Label("Active checklists")).
+			Body("Some content"),
+	).Add(
+		mtweb.NewCard().Header(mtweb.Icon("user-check").Label("My issues")).
+			Body("Some content"),
+	).Add(
+		mtweb.NewCard().Header(mtweb.Icon("chart-simple").Label("Statistics")).Body(renderStatistics()),
+	).Add(
+		mtweb.NewCard().Header(mtweb.Icon("cog").Label("System management")).
+			Body(dhtml.Div().Append(
+				dhtml.NewLink("/admin/users").Label(mtweb.Icon("user").Label("Users")),
+			)).
+			Body(dhtml.Div().Append(
+				dhtml.Div().Append(
+					dhtml.NewLink("/admin/checklists").Label(mtweb.Icon("list-check").Label("Checklists")),
+				).Append(" (administration)"),
+			)),
+	)
 
 	p.Main(cards_list)
 	return true
+}
+
+func renderStatistics() (out dhtml.HtmlPiece) {
+	out.Append(dhtml.RenderValue("User count", len(model.GetUsersList())))
+	out.Append(dhtml.RenderValue("Checklist count", len(model.GetChecklistsList())))
+
+	return out
 }
 
 func PageLogin(p *PageBuilder) bool {
