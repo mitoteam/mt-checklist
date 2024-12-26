d576825ceea06c44285b66e0bbee57b19987e432
Mikhail Oleynik <mishutka@mito-team.com>
2024-12-26 15:27:28 +0300

    experimants removed

--- main.go
+++ main.go
@@ -1,10 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-
 	"github.com/mitoteam/mbr"
 	"github.com/mitoteam/mt-checklist/app"
 	"github.com/mitoteam/mt-checklist/web"
@@ -12,21 +8,7 @@ import (
 
 func main() {
 	app := app.InitApp()
-	app.SetHandler(mbr.Handler(web.Root))
-
-	mbr.Dump()
+	app.SetHandler(mbr.Handler(web.RootCtl))
 
 	app.Run()
-}
-
-func TestMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("TestMiddleware: %s\n", r.RequestURI)
-
-		if ctx := mbr.Context(r); ctx != nil {
-			fmt.Printf("mbr.Route %s => %s\n", ctx.Route().Name(), ctx.Route().FullPath())
-		}
-
-		next.ServeHTTP(w, r)
-	})
 }
