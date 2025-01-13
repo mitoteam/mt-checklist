6adc7ed3a512efa9ad2237ecc6782818564ae4fe
Mikhail Oleynik <mishutka@mito-team.com>
2025-01-13 11:08:18 +0300

    active users only in dropdown

--- web/form_helpers.go
+++ web/form_helpers.go
@@ -11,7 +11,7 @@ func NewUserSelect(name string) *dhtmlbs.SelectFormControlElement {
 	selectControl := dhtmlbs.NewSelect(name)
 
 	//add users
-	goapp.PreQuery[model.User]().Order("user_name")
+	goapp.PreQuery[model.User]().Where("is_active", 1).Order("user_name")
 	for _, user := range goapp.LoadOL[model.User]() {
 		selectControl.Option(mttools.AnyToString(user.ID), user.GetDisplayName())
 	}
